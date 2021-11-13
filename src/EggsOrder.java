ee31da5f803f7a4d5333631d0dfa14126c9d13b7
Kyle Watson <kyle.p.watson@gmail.com>
2021-11-13 21:29:05 +0800

    Minor changes.
    Updated commenting.
    System will correctly output an error when 0 eggs are ordered.

--- src/EggsOrder.java
+++ src/EggsOrder.java
@@ -1,36 +1,41 @@
+
 import java.util.Scanner;
 
 public class EggsOrder {
+
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in); //user input through Scanner
-        // Declare some variables
+
+        //Creation of Scanner object and return of input stream.
+        Scanner sc = new Scanner(System.in);
+        //Variable Declaration
         int eggs;
         double totalCost;
         int amountOfDozen;
         int amountOfEggs;
 
-        System.out.print("How many eggs do you want to buy? : ");
-        //error trapping - if input not integer
+        System.out.print("How many eggs would you like to purchase? : ");
+        //Error trapping - if input entered is not int
         while (!sc.hasNextInt()) {
-            System.out.print("Please input number value, How many eggs do you want to buy? : ");
+            System.out.print("Please correctly input a number value, how many eggs do you wish to purchase? : ");
             sc.nextLine();
         }
+        //User requested number of eggs requested is assigned to the egg variable.
         eggs = sc.nextInt();
-
-        if(eggs >= 0) {
-            amountOfDozen = eggs / 12;  
+        //Nested if else block to output the total cost of the eggs requested.
+        if (eggs > 0) {
+            amountOfDozen = eggs / 12;
             amountOfEggs = eggs % 12;
             totalCost = (amountOfDozen * 7.25) + (amountOfEggs * 0.75);
             if (amountOfDozen == 0) {
-                System.out.println("You ordered " + eggs + " eggs. That is " + amountOfEggs + " eggs at $0.75 each for a total of $" + String.format("%.2f", totalCost));
+                System.out.println("You ordered " + eggs + " eggs. That is " + amountOfEggs + " eggs at $0.75 each for a total cost of $" + String.format("%.2f", totalCost) + ".");
             } else if (amountOfEggs == 0) {
-                System.out.println("You ordered " + eggs + " eggs. That is " + amountOfDozen + " dozen eggs at $7.25 per dozen for a total of $" + String.format("%.2f", totalCost));
+                System.out.println("You ordered " + eggs + " eggs. That is " + amountOfDozen + " dozen eggs at $7.25 per dozen for a total cost of $" + String.format("%.2f", totalCost) + ".");
             } else {
-                System.out.println("You ordered " + eggs + " eggs. That is " + amountOfDozen + " dozen eggs at $7.25 per dozen and " + amountOfEggs + " additional eggs at $0.75 each for a total of $" + String.format("%.2f", totalCost));
+                System.out.println("You ordered " + eggs + " eggs. That is " + amountOfDozen + " dozen eggs at $7.25 per dozen and " + amountOfEggs + " additional eggs at $0.75 each for a total cost of $" + String.format("%.2f", totalCost) + ".");
             }
+            //Error trapping - user must order at least one egg.
         } else {
-            System.out.println("Error! Please input plus number!"); //error trapping - if user input not plus value
+            System.out.println("Error! You must purchase at least one egg.");
         }
     }
 }

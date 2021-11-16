9a40d58274c5c887fd9e55cc14782dd158fbc98f
KrisTribbeck <kristribbeck@gmail.com>
2021-11-17 01:01:18 +0800

    Java Comments

--- src/EggsOrder.java
+++ src/EggsOrder.java
@@ -1,13 +1,30 @@
 
 import java.util.Scanner;
-
+/*
+* Classname: Java OOP
+* Team Name: Team 3
+* Programmers: Jae Kim, Kyle Watson and Kris Tribbeck.
+* Program description: This program prompts the user for the number of eggs 
+* in the order and then displays the amount owed with a full explanation.
+* 
+* MIT License - Copyright(c) 2021 Jae Kim, Kyle Watson and Kristiin Tribbeck.
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+*/
 public class EggsOrder {
 
     public static void main(String[] args) {
 
-        //Creation of Scanner object and return of input stream.
+        // Creation of Scanner object and return of input stream.
         Scanner sc = new Scanner(System.in);
-        //Variable Declaration
+        // Variable Declaration.
         int eggs = 0;
         double totalCost;
         int amountOfDozen;
@@ -15,14 +32,14 @@ public class EggsOrder {
 
         while (eggs == 0) {
             System.out.print("How many eggs would you like to purchase? : ");
-            //Error trapping - if input entered is not int
+            // Error trapping - if input entered is not an int.
             while (!sc.hasNextInt()) {
                 System.out.print("Please correctly input a number value, how many eggs do you wish to purchase? : ");
                 sc.nextLine();
             }
-            //User requested number of eggs requested is assigned to the egg variable.
+            // User requested number of eggs requested is assigned to the egg variable.
             eggs = sc.nextInt();
-            //Nested if else block to output the total cost of the eggs requested.
+            // Nested if else block to output the total cost of the eggs requested.
             if (eggs > 0) {
                 amountOfDozen = eggs / 12;
                 amountOfEggs = eggs % 12;
@@ -34,7 +51,7 @@ public class EggsOrder {
                 } else {
                     System.out.println("You ordered " + eggs + " eggs. That is " + amountOfDozen + " dozen eggs at $7.25 per dozen and " + amountOfEggs + " additional eggs at $0.75 each for a total cost of $" + String.format("%.2f", totalCost) + ".");
                 }
-                //Error trapping - user must order at least one egg.
+                // Error trapping - user must order at least one egg.
             } else {
                 System.out.println("Error! You must purchase at least one egg.");
             }
